7b723e3282c91a49313ae3784056e989697363cb
HarshaSai1234 <harshap3712@gmail.com>
2025-11-29 19:25:36 +0530

    Updated

--- src/Student_Managment_System/Main.java
+++ src/Student_Managment_System/Main.java
@@ -1,16 +1,14 @@
 package Student_Managment_System;
 import java.util.*;
 
-import static java.lang.System.exit;
-
 //Student details
 class Student {
-    int id;
+    String id;
     String name;
     int age;
     String branch;
     double cgpa;
-    public Student(int id,String name,int age,String branch,double cgpa){
+    public Student(String id,String name,int age,String branch,double cgpa){
         this.id=id;
         this.name=name;
         this.age=age;
@@ -33,19 +31,22 @@ class Functions {
     void addStudents() {
         System.out.println("Enter number of students to add: ");
         int n = sc.nextInt();
+        sc.nextLine();
         for(int i=0;i<n;i++) {
             System.out.println("Enter ID number of student: ");
-            int id = sc.nextInt();
+            String id = sc.nextLine();
             System.out.println("Enter name of student: ");
             String name=sc.nextLine();
             System.out.println("Enter age of student: ");
             int age = sc.nextInt();
+            sc.nextLine();
             System.out.println("Enter branch of student: ");
             String branch = sc.nextLine();
             System.out.println("Enter cgpa of student: ");
             double cgpa = sc.nextDouble();
+            sc.nextLine();
             list.add(new Student(id,name,age,branch,cgpa));
-            System.out.println("Student added succesfully..."+name);
+            System.out.println("Student added successfully..."+name);
         }
     }
     void displayAll() {
@@ -58,6 +59,24 @@ class Functions {
             }
         }
     }
+    void searchById(){
+        System.out.println("Enter Id: ");
+        String n=sc.nextLine();
+        boolean found= false;
+        for(Student s : list){
+            if(s.id.equals(n)){
+                System.out.println(s);
+                found=true;
+                break;
+            }
+        }
+        if(!found){
+            System.out.println("Student not found...");
+        }
+    }
+    void deleteStudentById(){
+
+    }
 
 }
 
@@ -69,20 +88,22 @@ public class Main {
         Functions f = new Functions();
         while(true) {
             System.out.println("Enter your choice: ");
-            System.out.println("1. Add Students\n2. Display all students\n3. Search Student by ID\n4. Delete Student by ID\n5. Update Student\n6. Exit");
+            System.out.println("1. Add Students\n2. Display all students\n3. Search Student by ID\n4. Delete Student by ID\n5. Update Student\n6. Delete all\n7. Exit");
             int choice=sc.nextInt();
             switch(choice) {
                 case 1: f.addStudents();
                     break;
                 case 2: f.displayAll();
                     break;
-                case 3: ;
+                case 3: f.searchById();
                     break;
-                case 4:
+                case 4: f.deleteStudentById();
                     break;
-                case 5: ;
+                case 5:
                     break;
-                case 6: System.out.println("Exiting....");
+                case 6:
+                    break;
+                case 7: System.out.println("Exiting....");
                         System.exit(0);
                     break;
                 default: System.out.println("Invalid choice...");
